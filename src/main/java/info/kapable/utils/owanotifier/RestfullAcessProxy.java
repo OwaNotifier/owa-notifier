5ec6a276ab1155d4812a5bc33ecd6848ec1cea30
Mathieu GOULIN <mathieu.goulin@gadz.org>
2017-08-23 23:18:42 +0200

    Dynamic read system properties about proxy

--- src/main/java/info/kapable/utils/owanotifier/RestfullAcessProxy.java
+++ src/main/java/info/kapable/utils/owanotifier/RestfullAcessProxy.java
@@ -23,19 +23,29 @@ SOFTWARE.
  */package info.kapable.utils.owanotifier;
 
 import info.kapable.utils.owanotifier.auth.TokenService;
+import info.kapable.utils.owanotifier.service.OutlookService;
 import info.kapable.utils.owanotifier.teststubs.StubTokenService;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Proxy;
+import java.net.ProxySelector;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.UUID;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import retrofit.RequestInterceptor;
 import retrofit.RestAdapter;
+import retrofit.RequestInterceptor.RequestFacade;
 import retrofit.RestAdapter.LogLevel;
 import retrofit.client.OkClient;
 
+import com.github.markusbernhardt.proxy.ProxySearch;
 import com.squareup.okhttp.OkHttpClient;
 
 /**
@@ -78,6 +88,95 @@ public class RestfullAcessProxy {
 	}
 	
 	/**
+	 * Return a proxy if exist
+	 * @return
+	 * 	null if no proxy, the InetSocketAddress else of proxy
+	 */
+	public static InetSocketAddress findProxy() {
+		logger.info("detecting proxies");
+		ProxySearch ps = ProxySearch.getDefaultProxySearch();
+		ProxySelector myProxySelector = ps.getProxySelector(); 
+		ProxySelector.setDefault(myProxySelector);
+
+
+		List<Proxy> l = null;
+		if(myProxySelector == null) {
+        	logger.info("No Proxy");
+			return null;
+		}
+		try {
+			l = myProxySelector.select(new URI("https://graph.microsoft.com"));
+		}  
+		catch (URISyntaxException e) {
+		    e.printStackTrace();
+		}
+		if (l != null) {
+		    for (Iterator<Proxy> iter = l.iterator(); iter.hasNext();) {
+		        java.net.Proxy proxy = (java.net.Proxy) iter.next();
+		        logger.info("proxy type: " + proxy.type());
+
+		        InetSocketAddress addr = (InetSocketAddress) proxy.address();
+		        if (addr == null) {
+		        	logger.info("No Proxy");
+		            return null;
+		        } 
+		        logger.info("Hostname: " + addr.getHostName());
+		        logger.info("Port : " + addr.getPort());
+		        return addr;
+		    }
+		}
+    	logger.info("No Proxy");
+		return null;
+	}
+	
+	public static OutlookService getRealOutlookService(String accessTokenSource, String userEmailSource) {
+		// Create a request interceptor to add headers that belong on
+		// every request
+		final String userEmail = userEmailSource;
+		final String accessToken = accessTokenSource;
+		RequestInterceptor requestInterceptor = new RequestInterceptor() {
+			@Override
+			public void intercept(RequestFacade request) {
+				request.addHeader("User-Agent", "owa-notifier-daemon");
+				request.addHeader("client-request-id", UUID.randomUUID().toString());
+				request.addHeader("return-client-request-id", "true");
+				request.addHeader("Authorization", String.format("Bearer %s", accessToken));
+				request.addHeader("Accept","application/json");
+
+				if (userEmail != null && !userEmail.isEmpty()) {
+					request.addHeader("X-AnchorMailbox", userEmail);
+				}
+			}
+		};
+		
+		OkHttpClient client = new OkHttpClient();
+		InetSocketAddress p = findProxy();
+		if(p != null) {
+			client.setProxy(new Proxy(Proxy.Type.HTTP,p));
+		} else { 
+			client.setProxy(Proxy.NO_PROXY); 
+		}
+		
+		// Create and configure the Retrofit object
+		RestAdapter restAdapter = new RestAdapter.Builder()
+				// Retrofit retrofit = new Retrofit.Builder()
+				.setEndpoint("https://graph.microsoft.com")
+				/*
+				 * .client(client)
+				 * .addConverterFactory(JacksonConverterFactory.create())
+				 */
+				.setRequestInterceptor(requestInterceptor).setLogLevel(LogLevel.FULL).setLog(new RestAdapter.Log() {
+					@Override
+					public void log(String msg) {
+						logger.debug(msg);
+					}
+				}).setClient(new OkClient(client)).build();
+
+		// Generate the token service
+		return restAdapter.create(OutlookService.class);
+	}
+	
+	/**
 	 * Return a instance of tokenService to call it
 	 * 
 	 * @param authority
@@ -86,21 +185,12 @@ public class RestfullAcessProxy {
 	private static TokenService getRealTokenService(String authority) {
 		// Create a logging interceptor to log request and responses
 		OkHttpClient client = new OkHttpClient();
-		String proxy;
-		try {
-			proxy = OwaNotifier.getProps().getProperty("proxyHost");
-			if (proxy != null && !proxy.contentEquals("")) {
-				int proxyPort = Integer.parseInt(OwaNotifier.getProps()
-						.getProperty("proxyPort", "0"));
-				if (proxyPort != 0) {
-					client.setProxy(new Proxy(Proxy.Type.HTTP,
-							new InetSocketAddress(proxy, proxyPort)));
-				}
-			}
-		} catch (IOException e1) {
-			e1.printStackTrace();
+		InetSocketAddress p = findProxy();
+		if(p != null) {
+			client.setProxy(new Proxy(Proxy.Type.HTTP,p));
+		} else { 
+			client.setProxy(Proxy.NO_PROXY); 
 		}
-
 		// Create and configure the Retrofit object
 		RestAdapter retrofit = new RestAdapter.Builder().setEndpoint(authority)
 				.setLogLevel(LogLevel.FULL).setLog(new RestAdapter.Log() {
@@ -112,6 +202,14 @@ public class RestfullAcessProxy {
 
 		// Generate the token service
 		return retrofit.create(TokenService.class);
+	}
+
+	/**
+	 * @return 
+	 * 	an instance of outlook service
+	 */
+	public static OutlookService getOutlookService(String accessTokenSource, String userEmailSource) {
+		return getRealOutlookService(accessTokenSource,userEmailSource);
 	}
 
 }
--- src/main/java/info/kapable/utils/owanotifier/service/OutlookServiceBuilder.java
+++ src/main/java/info/kapable/utils/owanotifier/service/OutlookServiceBuilder.java
@@ -22,22 +22,12 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
  */package info.kapable.utils.owanotifier.service;
 
-import info.kapable.utils.owanotifier.OwaNotifier;
+import info.kapable.utils.owanotifier.RestfullAcessProxy;
 
 import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.net.Proxy;
-import java.util.UUID;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import retrofit.RequestInterceptor;
-import retrofit.RestAdapter;
-import retrofit.RestAdapter.LogLevel;
-import retrofit.client.OkClient;
-
-import com.squareup.okhttp.OkHttpClient;
 
 public class OutlookServiceBuilder {
 	
@@ -45,50 +35,6 @@ public class OutlookServiceBuilder {
     private static Logger logger = LoggerFactory.getLogger(OutlookServiceBuilder.class);
 
 	public static OutlookService getOutlookService(String accessTokenSource, String userEmailSource) throws IOException {
-		// Create a request interceptor to add headers that belong on
-		// every request
-		final String userEmail = userEmailSource;
-		final String accessToken = accessTokenSource;
-		RequestInterceptor requestInterceptor = new RequestInterceptor() {
-			@Override
-			public void intercept(RequestFacade request) {
-				request.addHeader("User-Agent", "owa-notifier-daemon");
-				request.addHeader("client-request-id", UUID.randomUUID().toString());
-				request.addHeader("return-client-request-id", "true");
-				request.addHeader("Authorization", String.format("Bearer %s", accessToken));
-				request.addHeader("Accept","application/json");
-
-				if (userEmail != null && !userEmail.isEmpty()) {
-					request.addHeader("X-AnchorMailbox", userEmail);
-				}
-			}
-		};
-		
-		OkHttpClient client = new OkHttpClient();
-		String proxy = OwaNotifier.getProps().getProperty("proxyHost");
-		if(proxy != null && !proxy.contentEquals("")) {
-			int proxyPort = Integer.parseInt(OwaNotifier.getProps().getProperty("proxyPort", "0"));
-			if(proxyPort != 0) {
-				client.setProxy(new Proxy(Proxy.Type.HTTP, new InetSocketAddress(proxy, proxyPort)));
-			}
-		}
-
-		// Create and configure the Retrofit object
-		RestAdapter restAdapter = new RestAdapter.Builder()
-				// Retrofit retrofit = new Retrofit.Builder()
-				.setEndpoint("https://graph.microsoft.com")
-				/*
-				 * .client(client)
-				 * .addConverterFactory(JacksonConverterFactory.create())
-				 */
-				.setRequestInterceptor(requestInterceptor).setLogLevel(LogLevel.FULL).setLog(new RestAdapter.Log() {
-					@Override
-					public void log(String msg) {
-						logger.debug(msg);
-					}
-				}).setClient(new OkClient(client)).build();
-
-		// Generate the token service
-		return restAdapter.create(OutlookService.class);
+		return RestfullAcessProxy.getOutlookService(accessTokenSource, userEmailSource);
 	}
 }
