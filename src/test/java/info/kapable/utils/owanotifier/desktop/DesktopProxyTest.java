7351e5cb77533782bbd4b5f593959c9ec0cef834
Mathieu GOULIN <mathieu.goulin@gadz.org>
2017-08-23 18:00:05 +0200

    add Unit test for desktopProxy

--- src/test/java/info/kapable/utils/owanotifier/desktop/DesktopProxyTest.java
+++ src/test/java/info/kapable/utils/owanotifier/desktop/DesktopProxyTest.java
@@ -1,10 +1,17 @@
 package info.kapable.utils.owanotifier.desktop;
 
+import info.kapable.utils.owanotifier.InboxChangeEvent;
+import info.kapable.utils.owanotifier.service.Folder;
+
+import java.io.IOException;
+import java.util.Observable;
+
 import junit.framework.TestCase;
 
 import org.junit.Test;
 
 public class DesktopProxyTest extends TestCase {
+	public static InboxChangeEvent event;
 
 	@Test
 	public void testBroswe() {
@@ -15,6 +22,25 @@ public class DesktopProxyTest extends TestCase {
 			fail("MalformedURLException");
 		}
 		assertTrue(true);
+	}
+
+	@Test
+	public void testEventPocess() {
+		DesktopProxy c = new DesktopProxy() {
+			@Override
+			protected void processEvent(InboxChangeEvent event)
+					throws IOException {
+				DesktopProxyTest.event = event;
+			};
+		};
+		Folder folder = new Folder();
+		folder.setUnreadItemCount(1);
+		InboxChangeEvent arg = new InboxChangeEvent(folder,
+				InboxChangeEvent.TYPE_MANY_NEW_MSG);
+
+		c.update(new Observable(), arg);
+		assertTrue(DesktopProxyTest.event.getEventTitle().contains(
+				arg.getEventTitle()));
 	}
 
 }
